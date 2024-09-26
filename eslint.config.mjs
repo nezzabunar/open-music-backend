35d6fff1ccfb0f36aea20da8b714a663dac507b3
dev <nezzabunar@gmail.com>
2024-09-26 16:48:35 +0700

    upadate module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import daStyle from 'eslint-config-dicodingacademy';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from "eslint-config-dicodingacademy"
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   daStyle,
 ];
